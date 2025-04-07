a3c3697ab49e60318f919c9c3e7c26c58cdb6b7c
Arthur Ferreira dos Santos <Cleyton123fs@gmail.com>
2025-04-07 15:19:07 -0300

    incluindo ordenação da lista

--- git.js
+++ git.js
@@ -1,4 +1,5 @@
 var comidas =['Lasanha','Pizza','Sushi','Hamburguer']
 console.log(`Lista de comidas: ${comidas}`);
 console.log(`Primeira comida: ${comidas[0]}`);
-
+comidas.sort();
+console.log(`Comidas ordenadas: ${comidas}`);
